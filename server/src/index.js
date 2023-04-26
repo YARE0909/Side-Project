92ad41a04fd1ec031ee6a7a1e16855d1f31e8770
YARE0909 <pradyumnad092004@gmail.com>
2023-04-26 16:06:46 +0530

    ✨ Added signUp and signIn routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const prisma = require("../utils/prisma.js");
+const requireAuth = require("./middlewares/requireAuth.js");
+const authRoutes = require("./routes/authRoutes");
+const bodyParser = require("body-parser");
 var cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 app.use(cors());
 
@@ -20,7 +26,7 @@ mongoose.connection.on("error", (err) => {
   console.error("Error connecting to mongo", err);
 });
 
-app.get("/", (req, res) => {
+app.get("/", requireAuth, (req, res) => {
   res.send("Hello There");
 });
 
