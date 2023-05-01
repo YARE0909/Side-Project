99d93f0045224ce720373e9845ceeeee074c5101
YARE0909 <pradyumnad092004@gmail.com>
2023-05-01 23:04:12 +0530

    💄 Updated signIn UI

--- client/src/pages/signIn.tsx
+++ client/src/pages/signIn.tsx
@@ -54,42 +54,56 @@ const SignIn = () => {
     }
   }, []);
   return (
-    <div className="w-full min-h-screen h-full flex items-center justify-center bg-[#081c29] text-white">
-      <div className="w-fit h-fit flex justify-center items-center gap-14 bg-[#12a5ea] rounded-lg p-8">
+    <div className="w-full min-h-screen h-full flex flex-col gap-20 items-start justify-center bg-[#534c98] text-white">
+      <div className="pl-12">
+        <h1 className="text-8xl lg:text-9xl text-[#FFA3FD] font-bold">
+          chirp.
+        </h1>
+      </div>
+      <div className="w-fit h-fit flex flex-col justify-center items-center gap-14 rounded-lg p-8 pl-20">
         <form
           onSubmit={handleLogin}
-          className="flex flex-col justify-center items-start gap-5"
+          className="flex flex-col justify-center items-start gap-3 scale-[1.25]"
         >
-          <div className="input-container w-full">
-            <input onChange={handleChange} type="email" name="email" required />
-            <label className="block mb-2 text-sm font-medium text-white">
-              Email
-            </label>
-          </div>
-          <div className="input-container w-full">
-            <input
-              onChange={handleChange}
-              type="password"
-              name="password"
-              required
-            />
-            <label className="block mb-2 text-sm font-medium text-white">
-              Password
-            </label>
-          </div>
-          <button
-            type="submit"
-            className="relative inline-flex items-center justify-center p-0.5 overflow-hidden font-medium text-bg rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-200"
-          >
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-bg text-white font-bold rounded-md group-hover:bg-opacity-0">
-              Login
-            </span>
-          </button>
-          {!error ? null : (
-            <div>
-              <h1 className="text-red-600">{error}</h1>
+          <div>
+            <div className="input-container w-full">
+              <input
+                onChange={handleChange}
+                type="email"
+                name="email"
+                required
+              />
+              <label className="block mb-2 text-sm font-bold text-white">
+                Email
+              </label>
             </div>
-          )}
+            <div className="input-container w-full">
+              <input
+                onChange={handleChange}
+                type="password"
+                name="password"
+                required
+              />
+              <label className="block mb-2 text-sm font-bold text-white">
+                Password
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="border-[#FFEE7D] text-[#FFEE7D] font-bold border-2 rounded-lg pl-4 pr-4 pt-2 pb-2 hover:bg-[#FFEE7D] hover:text-[#b767ff] duration-300"
+            >
+              Login
+            </button>
+          </div>
+          <div>
+            {!error ? null : (
+              <div>
+                <h1 className="text-red-500 text-xs font-bold p-0 m-0">
+                  {error}
+                </h1>
+              </div>
+            )}
+          </div>
         </form>
       </div>
     </div>
